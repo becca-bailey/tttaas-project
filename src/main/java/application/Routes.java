83df9391f4ebd658bb6335d3541676e5b3613dc1
Becca Nelson <beccanelson88@gmail.com>
2016-07-18 10:50:16 -0500

    Import server as .jar and set up Controller and Routes from interface

---
+++ src/main/java/application/Routes.java
@@ -0,0 +1,11 @@
+package application;
+
+import com.server.routing.RouteInitializer;
+import com.server.routing.Router;
+
+public class Routes implements RouteInitializer {
+
+    public void initializeRoutes(Router router) {
+        
+    }
+}
---
+++ src/main/java/application/controllers/AbstractController.java
@@ -0,0 +1,38 @@
+package application.controllers;
+
+import com.server.ResponseData;
+import com.server.controller.Controller;
+
+public class AbstractController implements Controller {
+    public byte[] get() {
+        return new byte[0];
+    }
+
+    public byte[] head() {
+        return new byte[0];
+    }
+
+    public byte[] post() {
+        return new byte[0];
+    }
+
+    public byte[] put() {
+        return new byte[0];
+    }
+
+    public byte[] patch() {
+        return new byte[0];
+    }
+
+    public byte[] options() {
+        return new byte[0];
+    }
+
+    public byte[] delete() {
+        return new byte[0];
+    }
+
+    public void sendResponseData(ResponseData responseData) {
+
+    }
+}

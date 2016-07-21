c532d11e88bf489256c02809bfbac7af12f84dc8
Becca Nelson <beccanelson88@gmail.com>
2016-07-21 14:49:48 -0500

    Print request and error in controller

--- src/test/java/GameControllerTest.java
+++ src/test/java/GameControllerTest.java
@@ -1,6 +1,7 @@
 import application.controller.GameController;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -16,5 +17,26 @@ public class GameControllerTest {
     public void hasWinnerReturnsFalseForInitialBoard() throws Exception {
         String[] initialBoard = new String[] {"", "", "", "", "", "", "", "", ""};
         assertFalse(controller.hasWinner(initialBoard));
+    }
+
+    @Test
+    public void getGameStatusReturnsWinner() {
+        Boolean hasWinner = true;
+        Boolean isTie = false;
+        assertEquals("win", controller.getGameStatus(hasWinner, isTie));
+    }
+
+    @Test
+    public void getGameStatusReturnsTie() {
+        Boolean isTie = true;
+        Boolean hasWinner = false;
+        assertEquals("tie", controller.getGameStatus(hasWinner, isTie));
+    }
+
+    @Test
+    public void getGameStatusReturnsGameInProgress() {
+        Boolean isTie = false;
+        Boolean hasWinner = false;
+        assertEquals("in progress", controller.getGameStatus(hasWinner, isTie));
     }
 }
